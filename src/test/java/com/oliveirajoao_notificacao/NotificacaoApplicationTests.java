62283f5d5111cd158f330003aceeaa99a13bc221
Joao <joaovppo@gmail.com>
2025-11-17 09:44:35 -0300

    commit incial

---
+++ src/test/java/com/oliveirajoao_notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveirajoao_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
